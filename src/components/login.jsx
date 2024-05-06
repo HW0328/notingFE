8db5344d365d7036e30ee1bc8088db4bc865c325
HW0328 <115335241+HW0328@users.noreply.github.com>
2024-05-06 21:13:45 +0900

    init

---
+++ src/components/login.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Login = ({ be }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post(`${be}/log_in/`, {
+                id: username,
+                pw: password
+            });
+            console.log(response.data.isLogined); // 로그인 성공 시 데이터 출력
+            
+            if (response.data.isLogined === 1) {
+                const login_data = { 
+                    id : username,
+                    pw : password    
+                }
+                localStorage.setItem('login_data', JSON.stringify(login_data))
+                window.location.href = '/';
+                // 가져오기 : JSON.parse(localStorage.getItem('login_data'));
+            } else {
+                alert('The ID or password does not match.')
+                setUsername('');
+                setPassword('');
+            }
+            
+        } catch (error) {
+            if (error.response) {
+                setError(error.response.data); // 서버에서 반환한 오류 메시지 설정
+            } else {
+                setError('Network error'); // 네트워크 오류 발생 시 메시지 설정
+            }
+        }
+    };
+
+        return (
+            <div>
+            {error && <div>{error}</div>}
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                /> <br />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                /> <br />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/signUp.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+
+const SignUp = ({ be }) => {
+    const [id, setId] = useState('');
+    const [pw1, setPw1] = useState('');
+    const [pw2, setPw2] = useState('');
+
+    const signup = async (e) => {
+        e.preventDefault(); // 폼 제출 방지
+        
+        if (id === '' | pw1 === '' | pw2 === ''){
+            alert('Please fill out all input fields')
+            setId('');
+            setPw1('');
+            setPw2('');
+        } else if (pw1 !== pw2) {
+            alert('Two passwords do not match');
+            setPw1('');
+            setPw2('');
+        } else {
+            const data = {
+                id: id,
+                pw: pw1
+            }
+
+            try {
+                await axios.post(be + "/sign_up/", data)
+                .then(res => {
+                        if (res.data.err === 'None') { 
+                            window.location.href = '/'
+                        } else {
+                            alert(res.data.err)
+                        }
+                    }
+                )
+
+            } catch (error) {
+                console.error(error);
+                // 여기서 에러 처리 로직 추가 가능
+            }
+        }
+    }
+
+
+    return (
+        <div>
+            <form method="POST" onSubmit={signup} id="new">
+                
+                <input type="text" name="id" id="signup" placeholder="Please insert name." 
+                value={id} 
+                onChange={(e) => setId(e.target.value)}/>
+                
+                <br />
+                <input type="password" name="pw" id="signup" placeholder="Please insert password."
+                value={pw1} 
+                onChange={(e) => setPw1(e.target.value)}
+                /><br />
+                <input type="password" name="pw" id="signup" placeholder="Please insert your password again"
+                value={pw2} 
+                onChange={(e) => setPw2(e.target.value)}
+                /><br />
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default SignUp;
